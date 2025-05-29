bdb6a145570e5486da91ba61c8cbb53692f7b197
Cleiton Barbosa <Cleiton4792bar@gmail.com>
2025-05-29 15:17:22 -0300

    Add files via upload

---
+++ Atividade.sql
@@ -0,0 +1,10 @@
+create database projetojava2;
+
+use projetojava2;
+
+create table professor(
+codigo int primary key auto_increment,
+nome varchar(100));
+
+insert into professor(nome) values("José");
+
